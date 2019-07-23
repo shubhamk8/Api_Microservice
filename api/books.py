2c6d53a6c017392253c613f9c0eac3fc46c49a35
Janhavi Sathe <32736553+janhavi-sathe@users.noreply.github.com>
2019-07-23 12:39:29 +0530

    naming changes
    changes due to naming changes in Book model

--- api/books.py
+++ api/books.py
@@ -5,7 +5,7 @@ from flask import jsonify, request, Response
 
 
 def validate(bookObject):
-    if "name" in bookObject and "price" in bookObject and "ïsbn" in bookObject and "author" in bookObject:
+    if "title" in bookObject and "price" in bookObject and "isbn" in bookObject and "author" in bookObject:
         return True
     else:
         return True
@@ -24,22 +24,22 @@ def get_inventory():
 def add_book():
     request_data = request.get_json()
     if validate(request_data):
-        Book.add(request_data['name'], request_data['price'], request_data['author'], request_data['isbn'])
+        Book.add(request_data['title'], request_data['price'], request_data['author'], request_data['isbn'])
         resp = Response("", status=201, mimetype='application/json')
-        resp.headers['Location'] = "/inventory/" + str(request_data['name'])
+        resp.headers['Location'] = "/inventory/" + str(request_data['title'])
         return resp
     else:
         invalidBookObjectError = {
             "error": "Invalid book object passed in request",
-            "helpString": "Data is passed is similar to this {'name':'bookname', 'price':299, 'isbn':123453423}"
+            "helpString": "Data is passed is similar to this {'title':'bookname', 'price':299, 'isbn':123453423}"
         }
         response = Response(jsonify(invalidBookObjectError), status=400, mimetype='application/json')
         return response
 
 
-@bp.route('/inventory/<string:name>')
-def get_book_isbn(name):
-    book = Book.get_book(name)
+@bp.route('/inventory/<string:title>')
+def get_book_isbn(title):
+    book = Book.get_book(title)
     if book is None:
         invalidBookMsg = {
             "error": "Book with provided Name is not found"
@@ -52,9 +52,9 @@ def get_book_isbn(name):
         return response
 
 
-@bp.route('/inventory/<string:name>/locations')
-def get_book_location(name):
-    locations = db.session.query(Inventory.pincode).filter(Book.book_name == name).filter(
+@bp.route('/inventory/<string:title>/locations')
+def get_book_location(title):
+    locations = db.session.query(Inventory.pincode).filter(Book.title == title).filter(
         Location.pincode == Inventory.pincode).filter(Inventory.book_id == Book.book_id).all()
     location_schema = LocationSchema(many=True)
     response = Response(location_schema.dumps(locations).data, status=201, mimetype='application/json')
@@ -62,15 +62,15 @@ def get_book_location(name):
 
 
 @bp.route('/inventory/<string:name>/<int:pincode>')
-def check_availability(name, pincode):
-    result = Inventory.is_available(name, pincode)
+def check_availability(title, pincode):
+    result = Inventory.is_available(title, pincode)
     if result is not None:
         message = {
-            "msg": "This Book is available for ordering"
+            "msg": "This Book is available for ordering! :)"
         }
         response = Response(json.dumps(message), status=200, mimetype='application/json')
         return response
     else:
-        message = {"msg": "Sorry!!.. This Book is not available at your location"}
+        message = {"msg": "Sorry! This Book is currently unavailable in your location. :("}
         response = Response(json.dumps(message), status=200, mimetype='application/json')
         return response
